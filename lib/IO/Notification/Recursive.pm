b2f24c7f96e6a41beadd3c2571014484ea0e4bc3
Zoffix Znet <cpan@zoffix.com>
2016-04-29 12:02:44 -0400

    Fix compile issues

--- lib/IO/Notification/Recursive.pm
+++ lib/IO/Notification/Recursive.pm
@@ -3,15 +3,15 @@ unit module IO::Notification::Recursive;
 use File::Find;
 
 sub watch-recursive(Str $path, Bool :$update) is export {
-    my @dirs := find(:dir($path), :type<dir>);      # Get all subdirectories so we can watch them too.
-    my @paths := [ $path,  flat @dirs.map({ ~$_ }) ];
+    my @dirs = find(:dir($path), :type<dir>);      # Get all subdirectories so we can watch them too.
+    my @paths = [ $path, slip @dirs.map({ ~$_ }) ];
     supply {
         my sub watch-it($p) {
             whenever IO::Notification.watch-path($p) -> $e {
                 if $update && $e.event ~~ FileRenamed && $e.path.IO ~~ :d {
                     watch-it($e.path);
                 }
-                supply-emit($e);
+                emit($e);
             }
         }
         watch-it($_) for @paths;

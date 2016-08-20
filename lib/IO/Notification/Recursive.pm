64a8704570826d821217b74cc08c8b37e7dc6766
Jonathan Scott Duff <duff@pobox.com>
2016-08-20 14:09:36 -0500

    revamp

--- lib/IO/Notification/Recursive.pm
+++ lib/IO/Notification/Recursive.pm
@@ -3,19 +3,17 @@ unit module IO::Notification::Recursive;
 use File::Find;
 
 sub watch-recursive(Str $path, Bool :$update) is export {
-    supply {
-        my sub watch-it($p) {
-            whenever IO::Notification.watch-path($p) -> $e {
-                if $update && $e.event ~~ FileRenamed && $e.path.IO ~~ :d {
-                    watch-it($_) for find-dirs $e.path;
-                }
-                emit($e);
-            }
-        }
-        watch-it(~$_) for find-dirs $path;
-    }
-}
 
-my sub find-dirs (Str:D $p) {
-    return slip $p.IO, slip find :dir($p), :type<dir>;
+    my sub watch-dirs {
+        my @paths = $path, slip find :dir($path), :type<dir>;
+        return Supply.merge(map { IO::Notification.watch-path($_) }, @paths>>.Str);
+    }
+
+    my $watchers = watch-dirs;
+    supply {
+        whenever $watchers -> $e {
+            $watchers = watch-dirs if $update && $e.path.IO ~~ :d;
+            emit($e); 
+        }
+    }
 }
